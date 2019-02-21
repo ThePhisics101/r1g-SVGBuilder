be86e0eb062321a09a48b9ef4b8775c85573fbab
Pavils Jurjans <passiday@gmail.com>
2019-02-21 23:17:11 +0200

    Add the basic shape methods.

--- js/SVGBuilder.js
+++ js/SVGBuilder.js
@@ -1,6 +1,5 @@
 function SVGBuilder() {
-  this.document = document.createElement("div");
-  this.document.innerHTML = "SVG"
+  this.document = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 }
 
 SVGBuilder.prototype.insert = function(container, wipe) {
@@ -10,4 +9,24 @@ SVGBuilder.prototype.insert = function(container, wipe) {
     }
   }
   container.appendChild(this.document);
+}
+
+SVGBuilder.prototype.addRect = function(attr) {
+  // Add new rect element
+  console.log("Adding rect element, attr=", attr);
+}
+
+SVGBuilder.prototype.addCircle = function(attr) {
+  // Add new circle element
+  console.log("Adding circle element, attr=", attr);
+}
+
+SVGBuilder.prototype.addEllipse = function(attr) {
+  // Add new ellipse element
+  console.log("Adding ellipse element, attr=", attr);
+}
+
+SVGBuilder.prototype.addLine = function(attr) {
+  // Add new line element
+  console.log("Adding line element, attr=", attr);
 }
